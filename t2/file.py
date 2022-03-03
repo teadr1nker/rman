aa38af9b5aa5d800bc94ae652a40a7a4711f575b
tea <arsmir999@gmail.com>
2022-03-03 14:18:15 +0300

    t1

--- t2/file.py
+++ t2/file.py
@@ -16,14 +16,15 @@ def income(rts):
         r[i] = (rts[i+1] - rts[i]) / rts[i]
     return r
 
-df = pd.read_csv('candles1.csv', delimiter=';')
-df1 = pd.read_csv('candles2.csv', delimiter=';')
-df2 = pd.read_csv('candles3.csv', delimiter=';')
+df = pd.read_csv('candels1.csv', delimiter=';')
+df1 = pd.read_csv('candels2.csv', delimiter=';')
+df2 = pd.read_csv('candels3.csv', delimiter=';')
 RTS = np.array(df[['close']]).T[0]
 RTS1 = np.array(df1[['close']]).T[0]
 RTS2 = np.array(df2[['close']]).T[0]
 
 #1
+print('1) Доходность')
 R = income(RTS)
 R1 = income(RTS1)
 R2 = income(RTS2)
@@ -35,6 +36,7 @@ plt.legend(('R', 'R1', 'R2'))
 plt.savefig('plot1.png')
 plt.clf()
 #2
+print('\n2) Регрессия')
 a, b = np.polyfit(R, R1, 1)
 c, d = np.polyfit(R, R2, 1)
 print(f'beta1: {a} beta2: {c}')
@@ -44,6 +46,7 @@ plt.legend((f'beta1:{round(a, 2)}', f'beta2:{round(c, 2)}'))
 plt.savefig('plot2.png')
 plt.clf()
 #3
+print('\n3) Оценки рисков')
 residue1 = R1 - np.array([a * x + b for x in R])
 residue2 = R2 - np.array([c * x + d for x in R])
 risk1 = residue1.var()
@@ -51,13 +54,16 @@ risk2 = residue2.var()
 risk = R.var()
 print(f'risk: {risk} risk1: {risk1} risk2: {risk2}')
 #4
+print('\n4)')
 #def delta(arr):
 #    x1 = arr[0]; x2 = arr[1]
 #    return
 
 #5
+print('\n5)')
 
 #6
+print('\n6) Значимость')
 from sklearn.metrics import r2_score
 r21 = r2_score(R1 ,[a * x + b for x in R])
 r22 = r2_score(R2 ,[c * x + d for x in R])
@@ -65,6 +71,7 @@ print(f'r squared for r1: {r21}, root: {np.sqrt(r21)}')
 print(f'r squared for r2: {r22}, root: {np.sqrt(r22)}')
 
 #7
+print('\n7) Гетероскедастичность')
 sp1 = scipy.stats.spearmanr(R, R1)
 t1 = t((1 - sp1[1]) / 2, 13 - 2 - 1)
 sp2 = scipy.stats.spearmanr(R, R2)
@@ -72,6 +79,7 @@ t2 = t((1 - sp2[1]) / 2, 13 - 2 - 1)
 print(f'sp1 {t1 < sp1[1]}')
 print(f'sp2 {t2 < sp2[1]}')
 #8
+print('\n8) Дарбин-Уотсон')
 from statsmodels.stats.stattools import durbin_watson
 test1 = durbin_watson(residue1)
 test2 = durbin_watson(residue1)
